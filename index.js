300f72a9a9bca13314d0156e5ecece22a3c9d2cb
Phillupz <phillup.zukowski@gmail.com>
2022-03-18 20:34:07 -0400

    commit tree 33afbfb2125a4355df44bdf58217bd27de3c5eb5

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching (drivers, name) {
+    const found = drivers.filter(element => element.toUpperCase() === name.toUpperCase())
+     return found
+}
+
+function fuzzyMatch(drivers, letter) {
+    const find = drivers.filter(string => string.startsWith(letter))
+            return find
+}
+
+function matchName (drivers, string) {
+    const match = drivers.filter(drivers => drivers.name === string)
+    return match
+}
